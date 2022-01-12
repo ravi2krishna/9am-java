28284e00ca0734be384ccb42c78f1d8fe2d78181
ravi <ravi@ravis-MacBook-Pro.local>
2022-01-12 09:33:43 +0530

    COD Implemented

--- COD.java
+++ COD.java
@@ -1,5 +1,19 @@
 class COD {  
     public static void main(String args[]){  
-     System.out.println("Implement COD Functionality");  
+     System.out.println("Implement COD Functionality");
+     public static int convertBinaryToDecimal(long num) {
+    int decimalNumber = 0, i = 0;
+    long remainder;
+    
+    while (num != 0) {
+      remainder = num % 10;
+      num /= 10;
+      decimalNumber += remainder * Math.pow(2, i);
+      ++i;
+    }
+    
+    return decimalNumber;
+  }
+     System.out.println("Implemented COD Functionality");  
     }  
 }  
